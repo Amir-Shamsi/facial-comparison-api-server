ad40537adc6a18e414152555428c70174b970833
Amir Shamsi <emirshamsi.uni@gmail.com>
2021-09-25 10:43:23 +0330

    init fos rest routes

--- src/Controller/FRApiController.php
+++ src/Controller/FRApiController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * Class FRApiController
+ * @package App\Controller
+ * @Route ("/api", name="api_face_rec")
+ */
 class FRApiController extends AbstractController
 {
-
-    public function apiLauncher(): Response
+    /**
+     * @Rest\Post("/face-rec")
+     */
+    public function apiLauncher(Request $request): Response
     {
-        return $this->render('fr_api/index.html.twig', [
-            'controller_name' => 'FRApiController',
-        ]);
+
+    }
+
+    /**
+     * @Rest\Post("/face-rec/response")
+     */
+    public function apiResponse()
+    {
+
     }
 }
