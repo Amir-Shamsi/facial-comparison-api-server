8ccdcd01176713aeccdc058098f60a6d0b6e44b5
Amir Shamsi <emirshamsi.uni@gmail.com>
2021-09-25 10:46:57 +0330

    check csrf token

--- src/Controller/FRApiController.php
+++ src/Controller/FRApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,9 +20,19 @@ class FRApiController extends AbstractController
     /**
      * @Rest\Post("/face-rec")
      */
-    public function apiLauncher(Request $request): Response
+    public function apiLauncher(Request $request, LoggerInterface $logger): Response
     {
+        $token = $request->get("token");
 
+        if (!$this->isCsrfTokenValid('upload', $token))
+        {
+            $logger->info("CSRF failure");
+
+            return new Response("Operation not allowed",  Response::HTTP_BAD_REQUEST,
+                ['content-type' => 'application/json']);
+        }
+
+        
     }
 
     /**
