958459b01e2ab046ac5db72b645f8f1496d6c72f
Amir Shamsi <emirshamsi.uni@gmail.com>
2021-09-25 08:28:00 +0330

    add controller

---
+++ src/Controller/FRApiController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FRApiController extends AbstractController
+{
+
+    public function index(): Response
+    {
+        return $this->render('fr_api/index.html.twig', [
+            'controller_name' => 'FRApiController',
+        ]);
+    }
+}
