6ced30cfeafa9485f874fc9682239f90b5888f81
Amir Shamsi <emirshamsi.uni@gmail.com>
2021-09-26 15:44:10 +0330

    create FileService class

--- src/Services/FileService.php
+++ src/Services/FileService.php
@@ -4,24 +4,53 @@
 namespace App\Services;
 
 
+use phpDocumentor\Reflection\Types\This;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
 class FileService
 {
-    public static function destroySpaces(string $filename): string
+    public static function destroy_spaces(string $filename): string
     {
         return str_replace(' ', '_', $filename);
     }
 
     public static function getUniqueFilename(string $filename): string
     {
-        $filename = self::destroySpaces($filename);
-        return ($filename) . '_' . uniqid() . '.' . self::getFileFormat($filename);
+        $filename = self::destroy_spaces($filename);
+        return pathinfo($filename, PATHINFO_FILENAME) . '_' . uniqid() . '.' . self::get_file_format($filename);
     }
 
-    private static function getFileFormat(string $filename): string
+    public static function get_file_format(string $filename): string
     {
         $format = '';
-        for ($index = strlen($filename); $filename[$index] != '.'; $index--)
+        for ($index = strlen($filename)-1; $filename[$index] != '.'; $index--)
             $format .= strtolower($filename[$index]);
         return strrev($format);
     }
+
+    public static function create_file($path, $filename, $content): ?bool
+    {
+        try {
+            if (!file_exists($path)) {
+                mkdir($path, 0777, true);
+            }
+            file_put_contents($path.$filename, $content);
+            return true;
+        } catch (FileException $exception) {
+            return null;
+        }
+    }
+
+    public static function url_parse_filename(string $url): ?string
+    {
+        $name = '';
+        try {
+            for ($index = strlen($url)-1; $url[$index] != '\\' && $url[$index] != '/'; $index--)
+                $name .= strtolower($url[$index]);
+            return strrev($name);
+        } catch (\Exception $exception){
+            return null;
+        }
+    }
+
 }
