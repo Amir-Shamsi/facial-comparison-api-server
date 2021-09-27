98d551b808901956649e1c2c7c6b7cbfce8a78d4
Amir Shamsi <emirshamsi.uni@gmail.com>
2021-09-27 14:25:49 +0330

    update exec file

--- src/Services/FaceRecognition/face_recognition_.py
+++ src/Services/FaceRecognition/face_recognition_.py
@@ -1,53 +1,41 @@
 import sys
-sys.path.append("/")
-from facepplib import FacePP, exceptions
-face_detection = ""
-face_set_initialize = ""
-face_search = ""
-face_landmarks = ""
-dense_facial_landmarks = ""
-face_attributes = ""
-beauty_score_and_emotion_recognition = ""
-face_comparing_local_photo = ''
-face_comparing_website_photo = ''
+import json
+from face_recognition import FacePP, exceptions
+
+
 
 def face_comparing(app, Image1, Image2):
     cmp_ = app.compare.get(image_url1=Image1, image_url2=Image2)
-
-    print('Photo1', '=', cmp_.image1)
-    print('Photo2', '=', cmp_.image2)
-
     # Comparing Photos
     if cmp_.confidence > 70:
-        print(True+'&'+cmp_.confidence)
+        return {
+            "status" : True,
+            "percentage" : str(cmp_.confidence) + '%',
+            "errors" : None
+        }
     else:
-        print(False+'&'+cmp_.confidence)
+        return {
+            "status" : False,
+            "percentage" : str(cmp_.confidence) + '%',
+            "errors" : None
+        }
 
 if __name__ == '__main__':
-
     api_key = 'xQLsTmMyqp1L2MIt7M3l0h-cQiy0Dwhl'
     api_secret = 'TyBSGw8NBEP9Tbhv_JbQM18mIlorY6-D'
 
     try:
         app_ = FacePP(api_key=api_key, api_secret=api_secret)
-        funcs = [
-            face_detection,
-            face_comparing_local_photo,
-            face_comparing_website_photo,
-            face_set_initialize,
-            face_search,
-            face_landmarks,
-            dense_facial_landmarks,
-            face_attributes,
-            beauty_score_and_emotion_recognition
-        ]
-        image1 = 'https://nitratine.net/posts/python-face-recognition-tutorial/single-person.jpg'
-        image2 = 'https://nitratine.net/posts/python-face-recognition-tutorial/single-person.jpg'
+        image1 = sys.argv[1]
+        image2 = sys.argv[2]
 
-        # image1 = input()
-        # image2 = input()
-
-        face_comparing(app_, image1, image2)
-
+        response = face_comparing(app_, image1, image2)
     except exceptions.BaseFacePPError as e:
-        print('Error:', e)
+        response ={
+            'status' : None,
+            'percentage': None,
+            'errors' : e.args[0]
+        }
+json_object = json.dumps(response, indent = 3)
+
+print(json_object)
