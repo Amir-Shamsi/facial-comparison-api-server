978fa8b1de690eb8730d2c2235bd825736d083bb
Amir Shamsi <emirshamsi.uni@gmail.com>
2021-09-27 14:28:51 +0330

    exec and return json response

--- src/Services/FaceRecognition/FaceRecognition.php
+++ src/Services/FaceRecognition/FaceRecognition.php
@@ -4,22 +4,31 @@
 namespace App\Services\FaceRecognition;
 
 
+use phpDocumentor\Reflection\Types\This;
+use \Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class FaceRecognition
 {
-    public static function comparison(string $sourceFilename, string $targetFilename): Response
+    public static function comparison(string $sourceFilename, string $targetFilename, $prj_directory, Request $request): Response
     {
-        $command = escapeshellcmd('python face_recognition_.py '.$sourceFilename.' '.$targetFilename);
+
+        $py_file_path = $prj_directory.'/src/Services/FaceRecognition/'.'face_recognition_.py';
+        $command = escapeshellcmd('python '.
+            $py_file_path.' '.
+            $sourceFilename.' '.
+            $targetFilename
+        );
+
         $output = shell_exec($command);
-        $response = explode('&', $output);
-        $result = $response[0] === true || $response[0] === 'True' || $response[0] === 'true';
+        $json_response = json_decode($output);
+
         return new JsonResponse([
             'status' => Response::HTTP_OK,
             'result' => [
-                'comparison_result' => $result,
-                'percentage' => (int)$response[1]
+                'comparison_result' => $json_response->status,
+                'percentage' => $json_response->percentage
             ],
         ],
         Response::HTTP_OK,
